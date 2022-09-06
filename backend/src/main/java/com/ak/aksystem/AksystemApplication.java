1a2a3905e15fc970c8d82c12f767758b58794702
Leandro Luís Daronch <ldaronch@yahoo.com.br>
2022-09-05 21:19:27 -0300

    Projeto Spring React

---
+++ backend/src/main/java/com/ak/aksystem/AksystemApplication.java
@@ -0,0 +1,13 @@
+package com.ak.aksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AksystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ak/aksystem/AksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.aksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
